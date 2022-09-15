b299923b3bc9ae2994a219406d972f71d82152a2
Lilsliceofpie <bar.tynan@gmail.com>
2022-09-15 19:25:52 +0000

    added game basic framework

--- run.py
+++ run.py
@@ -1,3 +1,86 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Board:
+    """
+    Main Board class. Sets board size, the number of ships, 
+    the players name and the board type (player board or computer).
+    Has methods for adding ships and guesses and printing the board.
+    """
+
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+    def print(self):
+        for row in self.board:
+            print("".join(row))
+
+    def guess(self, x, y):
+        self.guesses.append((x, y))   
+        self.board[x][y] = "X"
+
+        if (x, y) in self.ships:  
+            self.board[x][y] = "*"
+            return "HIT!"
+        else:
+            return "MISS!"
+
+    def add_ship(self, x, y, type="computer"):
+        if len(self.ships) >= self.num_ships:
+            print("Error: you cannot add any more ships!")
+        else:
+            self.ships.append((x, y))
+            if self.type == "player":
+                self.board[x][y] = "@"
+
+
+def random_point(size):
+    """
+    Helper function to return a random integer between 0 and size
+    """
+    return randint(0, size - 1)
+
+
+def valid_coordinates(x, y, board):
+
+
+def populate_board(board):
+
+
+def make_guess(board)
+
+def play_game(computer_board, player_board)
+
+
+def new_game():
+    """
+    Starts a new game, sets the board size and number of ships, resets the
+    scores and initializes the boards.
+    """
+
+    size = 5
+    num_ships = 4
+    scores["computer"] = 0
+    scores["player"] = 0
+    print("-" * 35)
+    print("Welcome to ULTIMATE BATTLESHIPS!!")
+    print(f"Board size: {size}. Number of ships: {num_ships}")
+    print(" Top left corner is row: 0, col: 0")
+    print("-" * 35)
+    player_name = input("Please enter your name: \n")
+    print("-" * 35)
+
+    computer_board = Board(size, num_ships, "Computer", type="computer")
+    player_board = Board(size, num_ships, player_name, type="player")
+
+    for _ in range(num_ships):
+        populate_board(player_board)
+        populate_board(computer_board)
+
+    play_game(computer_board, player_board)
+
+
+new_game()
